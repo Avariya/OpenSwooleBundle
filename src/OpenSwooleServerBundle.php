5368d231e62dc2bc2478afa09c8ce90042775e52
Yevhen Ponomarenko <yev.ponomarenko@foodtech.team>
2024-05-14 09:45:53 +0000

    [TECH][PBG-11] Batch runner

--- src/OpenSwooleServerBundle.php
+++ src/OpenSwooleServerBundle.php
@@ -1,27 +1,27 @@
 <?php
+
 declare(strict_types=1);
 
 namespace OpenSwooleServerBundle;
 
 use OpenSwooleServerBundle\DependencyInjection\CompilerPass\DoctrineCleanerSubscriberPass;
+use OpenSwooleServerBundle\DependencyInjection\CompilerPass\LoggerCompilerPass;
 use OpenSwooleServerBundle\DependencyInjection\CompilerPass\SentryPublisherSubscriberPass;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Class OpenSwooleServerBundle
+ * Class OpenSwooleServerBundle.
  */
 class OpenSwooleServerBundle extends Bundle
 {
-    /**
-     * @param ContainerBuilder $container
-     */
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
         $container->addCompilerPass(new DoctrineCleanerSubscriberPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
         $container->addCompilerPass(new SentryPublisherSubscriberPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
+        $container->addCompilerPass(new LoggerCompilerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 100);
     }
 }
