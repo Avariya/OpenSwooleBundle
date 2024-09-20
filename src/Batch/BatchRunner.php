17ef137886d075e369b3d7ba6b015ea28d7013af
Oleksii Romanchenko <ol.romanchenko@foodtech.team>
2024-09-20 14:34:56 +0300

    [TECH] cast key to string for dispatcher

--- src/Batch/BatchRunner.php
+++ src/Batch/BatchRunner.php
@@ -168,19 +168,19 @@ final class BatchRunner
         $batchRunner = $this;
 
         return static function () use ($resultChannel, $key, $callable, $eventDispatcher, $batchRunner): void {
-            $eventDispatcher?->dispatch(new BatchRunnerItemStarted($batchRunner, $key));
+            $eventDispatcher?->dispatch(new BatchRunnerItemStarted($batchRunner, (string)$key));
 
             try {
                 $result = Result::fromValue($callable());
 
                 $eventDispatcher?->dispatch(
-                    new BatchRunnerItemEndedSuccessfully($batchRunner, $key)
+                    new BatchRunnerItemEndedSuccessfully($batchRunner, (string)$key)
                 );
             } catch (Throwable $e) {
                 $result = Result::fromThrowable($e);
 
                 $eventDispatcher?->dispatch(
-                    new BatchRunnerItemEndedWithException($batchRunner, $key, $e)
+                    new BatchRunnerItemEndedWithException($batchRunner, (string)$key, $e)
                 );
             }
             $resultChannel->push([$key, $result]);
